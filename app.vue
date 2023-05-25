bf26ce00dd0ea80dfa41607d82916ffb245f3af6
kuboryoya <kubo.ryoya822@gmail.com>
2023-05-25 20:46:01 +0900

    initial commit

---
+++ app.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+// no index
+useHead({
+  meta: [
+    {
+      name: "robots",
+      content: "noindex"
+    }
+  ]
+})
+</script>
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+
+export default defineNuxtConfig({
+
+  modules: ['@nuxtjs/tailwindcss'],
+  css: ['~/assets/css/main.scss'],
+})
